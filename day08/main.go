bdf0926ffd2de175541ef7a0c82a95bf4efbd584
Yves Brissaud <yves.brissaud@gmail.com>
2020-12-08 09:11:05 +0100

    day08 - part 2

--- day08/main.go
+++ day08/main.go
@@ -17,6 +17,8 @@ func main() {
 	b := NewBootSequence(string(content))
 	b.Run()
 	fmt.Println("acc:", b.Acc)
+	_ = b.FixAndRun()
+	fmt.Println("fixed:", b.Acc)
 }
 
 type BootSequence struct {
@@ -31,27 +33,39 @@ func NewBootSequence(input string) *BootSequence {
 }
 
 func (b *BootSequence) Run() {
-	b.Acc = 0
+	_, acc := run(b.instructions)
+	b.Acc = acc
+}
+
+func run(instructions []string) (bool, int) {
+	acc := 0
+	nextInstruction := len(instructions)
 	idx := 0
 	alreadyRunIdx := map[int]bool{}
+	eof := false
 
 	for {
+		if eof {
+			break
+		}
 		if _, ran := alreadyRunIdx[idx]; ran {
 			break
 		}
 		alreadyRunIdx[idx] = true
-		instruction := b.instructions[idx]
+		instruction := instructions[idx]
 
 		if strings.HasPrefix(instruction, "nop") {
 			idx++
+			eof = idx == nextInstruction
 			continue
 		}
 
 		if strings.HasPrefix(instruction, "acc") {
 			incStr := strings.TrimPrefix(instruction, "acc ")
 			inc, _ := strconv.Atoi(incStr)
-			b.Acc += inc
+			acc += inc
 			idx++
+			eof = idx == nextInstruction
 			continue
 		}
 
@@ -59,7 +73,38 @@ func (b *BootSequence) Run() {
 			jmpStr := strings.TrimPrefix(instruction, "jmp ")
 			jmp, _ := strconv.Atoi(jmpStr)
 			idx += jmp
+			eof = idx == nextInstruction
 			continue
 		}
 	}
+	return eof, acc
+}
+
+func (b *BootSequence) FixAndRun() bool {
+	fixInstructionAndRun := func(from []string, idx int, instruction, before, after string) (bool, int) {
+		instructions := make([]string, len(b.instructions))
+		copy(instructions, b.instructions)
+		newInstruction := after + strings.TrimPrefix(instruction, before)
+		instructions[idx] = newInstruction
+		return run(instructions)
+
+	}
+
+	for idx, instruction := range b.instructions {
+		if strings.HasPrefix(instruction, "nop") {
+			eof, acc := fixInstructionAndRun(b.instructions, idx, instruction, "nop ", "jmp ")
+			if eof {
+				b.Acc = acc
+				return true
+			}
+		}
+		if strings.HasPrefix(instruction, "jmp") {
+			eof, acc := fixInstructionAndRun(b.instructions, idx, instruction, "jmp ", "nop ")
+			if eof {
+				b.Acc = acc
+				return true
+			}
+		}
+	}
+	return false
 }
--- day08/main_test.go
+++ day08/main_test.go
@@ -19,3 +19,24 @@ acc +6`
 		t.Fatal(b.Acc, 5)
 	}
 }
+
+func TestFix(t *testing.T) {
+	const input = `nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6`
+
+	b := NewBootSequence(input)
+	success := b.FixAndRun()
+	if !success {
+		t.Fatal("inifite loop")
+	}
+	if b.Acc != 8 {
+		t.Fatal(b.Acc, 8)
+	}
+}
